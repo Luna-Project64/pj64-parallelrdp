142efea237136c7026fda9398b0fa02ffa911e28
aglab2 <aglab3@gmail.com>
2024-01-11 19:30:05 +0800

    Bring back headers with original license

--- src/retroarch/retroarch.c
+++ src/retroarch/retroarch.c
@@ -1,3 +1,24 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ *  Copyright (C) 2011-2021 - Daniel De Matteis
+ *  Copyright (C) 2012-2015 - Michael Lelli
+ *  Copyright (C) 2014-2017 - Jean-Andr� Santoni
+ *  Copyright (C) 2016-2019 - Brad Parker
+ *  Copyright (C) 2016-2019 - Andr�s Su�rez (input mapper code)
+ *  Copyright (C) 2016-2017 - Gregor Richards (network code)
+ *
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "retroarch.h"
 
 #include "../config.h"
